a1be9afb2d2cbf5717202751e70da2cb2383f627
Sami Koponen <samikoponen4@gmail.com>
2025-01-12 19:36:59 +0200

    T1

--- Viikko-1/T1/main.cpp
+++ Viikko-1/T1/main.cpp
@@ -1,25 +1,47 @@
 #include <iostream>
 
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
-    auto lang = "C++";
-    std::cout << "Hello and welcome to " << lang << "!\n";
+    //A)
+    int muuttuja{};
+    int* muuttuja_p {&muuttuja};
 
-    for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "i = " << i << std::endl;
-    }
+    //B)
+    *muuttuja_p = 7;
+
+    //C)
+    std::cout << "muuttuja_p = " << muuttuja_p << '\n';
+    std::cout << "muututja = " << muuttuja << '\n';
+
+    //D)
+    std::cout << "&muuttuja_p = " << &muuttuja_p << '\n';
+    std::cout << "&muuttuja = " << &muuttuja << '\n';
+
+    std::cout << "*&muuttuja = " << *&muuttuja << '\n';
+    std::cout << "&*muuttuja_p = " << &*muuttuja_p << '\n';
+
+
+
+    //E)
+    std::cout << "sizeof(muuttuja) = " <<  sizeof(muuttuja) << '\n';
+    std::cout << "sizeof(muuttuja_p) = " << sizeof(muuttuja_p) << '\n';
+
+    //G)
+    int muuttuja2{50};
+    int* muuttuja2_p{};
+    std::cout << "muuttuja2_p = " << *muuttuja2_p << '\n';
+
+    muuttuja2_p = nullptr;
+    std::cout << "muuttuja2_p = " << *muuttuja2_p << '\n';
+
+
+
+
+
+
+
+
+
+
 
     return 0;
 }
-
-// TIP See CLion help at <a
-// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
-//  Also, you can try interactive lessons for CLion by selecting
-//  'Help | Learn IDE Features' from the main menu.

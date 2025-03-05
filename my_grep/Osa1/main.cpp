eb3f2eaf091bb8750c1c476a529355d24ae5aedb
Sami Koponen <39089392+skopo97@users.noreply.github.com>
2025-03-05 14:36:34 +0200

    Update main.cpp
    Kommentteja.

--- my_grep/Osa1/main.cpp
+++ my_grep/Osa1/main.cpp
@@ -11,22 +11,33 @@ int main() {
   std::string searchString;
   std::getline(std::cin >> std::ws, searchString);
 
+  // Rakennetaan shell komento joka käyttää grep komentoa merkkijonon etsimiseen. -bo palauttaa indeksin ja etsityn merkkijonon
   std::string command = "echo " + inputString + " | grep -bo " + searchString;
 
+  //Avataan uusi prosessi ja luetaan sen tulos.
   FILE* file = popen(command.c_str(), "r");
+
+  //Tähän tallennetaan grepin antama tulos
   char start[32];
+  //aloitus positio -1, eli tulosta ei ole.
   int currentPos {-1};
 
+  //Jos proesssin avaaminen onnistui
   if (file != NULL) {
+    //Tämä lukee grepin antaman tuloksen, jos se onnistui. eli ei ole null.
     if (fgets(start, sizeof(start), file) != NULL) {
+      //tällä saadaan indeksi, josta se löytyi, koska atoi lukee rivin alusta siihen asti kunnes se ei löydä enää numeroa. Eli, jos
+      //tulos on esim 7:kissa, niin se lukee merkin 7 ja lopettaa. Tallennetaan se muuttuujaan currentPos
       currentPos = atoi(start);
     }
+    //Suljetaan prosessi
     pclose(file);
   }
 
-
+  //jos currentPois ei ole -1, eli tulosta ei ole niin kerrotaan, että löytyi ja mistä löytyi
   if (currentPos != -1) {
     std::cout << "\"" << searchString << "\" found in \"" << inputString << "\" at position " << currentPos << "\n";
+    //Muuten kerrotaan, että ei grep ei löytänyt mitään.
   } else {
     std::cout << "\"" << searchString << "\" NOT found in \"" << inputString
               << "\"\n";

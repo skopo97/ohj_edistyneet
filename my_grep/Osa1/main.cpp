e7bd30422f15d1e8bddc70af48ef12e13c61d339
Sami Koponen <39089392+skopo97@users.noreply.github.com>
2025-03-05 14:56:37 +0200

    main.cpp

--- my_grep/Osa1/main.cpp
+++ my_grep/Osa1/main.cpp
@@ -1,46 +1,27 @@
-#include <cstdlib>
 #include <iostream>
-#include <unistd.h>
+#include <string>
 
 int main() {
-  std::cout << "Give a string from which to search for : ";
-  std::string inputString;
-  std::getline(std::cin >> std::ws, inputString);
+    // Pyydetään käyttäjältä merkkijono
+    std::cout << "Give a string from which to search for : ";
+    std::string inputString;
+    std::getline(std::cin, inputString);
 
-  std::cout << "Give a search string: ";
-  std::string searchString;
-  std::getline(std::cin >> std::ws, searchString);
+    // Pyydetään käyttäjältä etsittävä merkkijono
+    std::cout << "Give a search string : ";
+    std::string searchString;
+    std::getline(std::cin, searchString);
 
-  // Rakennetaan shell komento joka käyttää grep komentoa merkkijonon etsimiseen. -bo palauttaa indeksin ja etsityn merkkijonon
-  std::string command = "echo " + inputString + " | grep -bo " + searchString;
+    // Etsitään alimerkkijonon ensimmäinen esiintymiskohta
+    size_t found = inputString.find(searchString);
 
-  //Avataan uusi prosessi ja luetaan sen tulos.
-  FILE* file = popen(command.c_str(), "r");
-
-  //Tähän tallennetaan grepin antama tulos
-  char start[32];
-  //aloitus positio -1, eli tulosta ei ole.
-  int currentPos {-1};
-
-  //Jos proesssin avaaminen onnistui
-  if (file != NULL) {
-    //Tämä lukee grepin antaman tuloksen, jos se onnistui. eli ei ole null.
-    if (fgets(start, sizeof(start), file) != NULL) {
-      //tällä saadaan indeksi, josta se löytyi, koska atoi lukee rivin alusta siihen asti kunnes se ei löydä enää numeroa. Eli, jos
-      //tulos on esim 7:kissa, niin se lukee merkin 7 ja lopettaa. Tallennetaan se muuttuujaan currentPos
-      currentPos = atoi(start);
+    if (found != std::string::npos) { // Tarkistetaan löytyikö merkkijono
+        std::cout << "\"" << searchString << "\" löytyi merkkijonosta \"" 
+                  << inputString << "\" kohdasta " << found << "\n";
+    } else {
+        std::cout << "\"" << searchString << "\" EI löytynyt merkkijonosta \"" 
+                  << inputString << "\"\n";
     }
-    //Suljetaan prosessi
-    pclose(file);
-  }
 
-  //jos currentPois ei ole -1, eli tulosta ei ole niin kerrotaan, että löytyi ja mistä löytyi
-  if (currentPos != -1) {
-    std::cout << "\"" << searchString << "\" found in \"" << inputString << "\" at position " << currentPos << "\n";
-    //Muuten kerrotaan, että ei grep ei löytänyt mitään.
-  } else {
-    std::cout << "\"" << searchString << "\" NOT found in \"" << inputString
-              << "\"\n";
-  }
-  return 0;
+    return 0;
 }

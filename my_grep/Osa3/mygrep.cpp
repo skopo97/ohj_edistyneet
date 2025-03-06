29227b88280e9d97afebbad9f1d2d57a098a13e7
Sami Koponen <39089392+skopo97@users.noreply.github.com>
2025-03-06 18:17:07 +0200

    päivitetty osa3

--- my_grep/Osa3/mygrep.cpp
+++ my_grep/Osa3/mygrep.cpp
@@ -4,30 +4,30 @@
 
 int main(int argc, char* argv[]) {
 
-    //jos komentoriviargumentteja ei ole, niin pysyt‰‰n ensimm‰isess‰ osassa, eli k‰ytt‰j‰n syˆtteess‰
+    //jos komentoriviargumentteja ei ole, niin pysyt√§√§n ensimm√§isess√§ osassa, eli k√§ytt√§j√§n sy√∂tteess√§
     if (argc == 1) {
 
-        std::cout << "Anna merkkijono, josta etsit‰‰n: ";
+        std::cout << "Anna merkkijono, josta etsit√§√§n: ";
         std::string inputString;
         std::getline(std::cin, inputString);
 
-        std::cout << "Anna etsitt‰v‰ merkkijono: ";
+        std::cout << "Anna etsitt√§v√§ merkkijono: ";
         std::string searchString;
         std::getline(std::cin, searchString);
 
         size_t found = inputString.find(searchString);
         if (found != std::string::npos) {
-            std::cout << "\"" << searchString << "\" lˆytyi merkkijonosta \""
+            std::cout << "\"" << searchString << "\" l√∂ytyi merkkijonosta \""
                 << inputString << "\" kohdasta " << found << "\n";
         }
         else {
-            std::cout << "\"" << searchString << "\" EI lˆytynyt merkkijonosta \""
+            std::cout << "\"" << searchString << "\" EI l√∂ytynyt merkkijonosta \""
                 << inputString << "\"\n";
         }
     }
-    //Jos parametrej‰ on 3
+
     else if (argc == 3) {
-        //Komentorivilt‰ saadut parametrit
+        //Komentorivilt√§ saadut parametrit
         //haettu teksti
         std::string searchString = argv[1];
         //tiedosto joka avataan
@@ -41,37 +41,88 @@ int main(int argc, char* argv[]) {
         }
 
         std::string line;
-        std::size_t lineNumber{ 1 };
-        int occurance{ 0 };
         bool found{ false };
 
-        // Luetaan tiedostoa rivi rivilt‰
+        // Luetaan tiedostoa rivi rivilt√§
         while (std::getline(file, line)) {
-            //Jos rivilt‰ lˆytyy etsitt‰v‰ teksti, niin tulostetaan se.
-            //find palauttaa npos arvon, jos se ei lˆyd‰ mit‰‰n. Eli voidaan verrata onko tulos eri. Jos on
-            //niin se tarkoittaa, ett‰ se lˆysi jotain.
-            
-             //Kasvatatetaan lineNumber muuttuujaa joka rivill‰
-            ++lineNumber;
+            //Jos rivilt√§ l√∂ytyy etsitt√§v√§ teksti, niin tulostetaan se.
+            //find palauttaa npos arvon, jos se ei l√∂yd√§ mit√§√§n. Eli voidaan verrata onko tulos eri. Jos on
+            //niin se tarkoittaa, ett√§ se l√∂ysi jotain.
+
 
             if (line.find(searchString) != std::string::npos) {
-                //jos vastaavaisuus lˆytyi, kasvatetaan occurance muuttujaa yhdell‰.
-                ++occurance;
-                std::cout << lineNumber << ":\t" << line << "\n";
+                std::cout << line << '\n';
                 found = true;
             }
 
         }
-
-        std::cout << "Occurrences of lines containing \"" << searchString << "\": " << occurance;
-        //Jos ei lˆytynyt.
+        //Jos ei l√∂ytynyt.
         if (!found) {
-            std::cout << "Merkkijonoa \"" << searchString << "\" ei lˆytynyt tiedostosta \"" << fileName << "\".\n";
+            std::cout << "Merkkijonoa \"" << searchString << "\" ei l√∂ytynyt tiedostosta \"" << fileName << "\".\n";
+
+        }
+    }
+    //Jos parametrej√§ on 4
+    else if (argc == 4) {
+        //Komentorivilt√§ saadut parametrit
+        // 
+        //Lippu
+        std::string flag = argv[1];
+        //haettu teksti
+        std::string searchString = argv[2];
+        //tiedosto joka avataan
+        std::string fileName = argv[3];
+
+
+        //Avataan tiedosto.
+        std::ifstream file(fileName);
+        if (!file) {
+            std::cerr << "Virhe: Tiedostoa \"" << fileName << "\" ei voitu avata.\n";
+            return 1;
+        }
+
+        std::string line;
+        std::size_t lineNumber{ 1 };
+        int occurance{ 0 };
+        bool found{ false };
+
+        // Luetaan tiedostoa rivi rivilt√§
+        while (std::getline(file, line)) {
+            //Jos rivilt√§ l√∂ytyy etsitt√§v√§ teksti, niin tulostetaan se.
+            //find palauttaa npos arvon, jos se ei l√∂yd√§ mit√§√§n. Eli voidaan verrata onko tulos eri. Jos on
+            //niin se tarkoittaa, ett√§ se l√∂ysi jotain.
+            
+             //Kasvatatetaan lineNumber muuttuujaa joka rivill√§
+            ++lineNumber;
+
+            if (line.find(searchString) != std::string::npos) {
+                //jos vastaavaisuus l√∂ytyi, kasvatetaan occurance muuttujaa yhdell√§.
+                ++occurance;
+
+                //Tarkastetaan lippu ja tulostetaan sen mukaan rivinumero tai ei
+                if (flag == "-ol" || flag == "-olo") {
+                    std::cout << lineNumber << ":\t" << line << "\n";
+                }
+                else {
+                    std::cout << line << '\n';
+                }
+                
+                found = true;
+            }
+
+        }
+        //Tarkastetaan lippu ja tulostetaan sen mukaan osumat tai ei
+        if (flag == "-oo" || flag == "-olo") {
+            std::cout << "Occurrences of lines containing \"" << searchString << "\": " << occurance << '\n';
+        }
+        //Jos ei l√∂ytynyt.
+        if (!found) {
+            std::cout << "Merkkijonoa \"" << searchString << "\" ei l√∂ytynyt tiedostosta \"" << fileName << "\".\n";
 
         }
     }
 
-    else if (argc == 2 || argc > 3) {
-        std::cout << "V‰‰r‰ m‰‰r‰ argumentteja\n";
+    else if (argc == 2 || argc > 4) {
+        std::cout << "V√§√§r√§ m√§√§r√§ argumentteja\n";
     }
 }

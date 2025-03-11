69db739f1b615223aa803befe970f7c834b13041
skopo97 <samikoponen4@gmail.com>
2025-03-11 15:05:54 +0200

    t2 kesken

---
+++ OOP-Viikko-1/T2/Paivays.h
@@ -0,0 +1,32 @@
+//
+// Created by samit on 3/11/2025.
+//
+
+#ifndef PAIVAYS_H
+#define PAIVAYS_H
+
+
+
+class Paivays {
+private:
+    int m_paiva{};
+    int m_kuukausi{};
+    int m_vuosi{};
+
+public:
+    //setterit
+    void setPaiva(const int& paiva);
+    void setKuukausi(const int& kuukausi);
+    void setVuosi(const int& vuosi);
+
+    //getterit
+    int getPaiva() const;
+    int getKuukausi() const;
+    int getVuosi() const;
+
+    void tulostaPaivays() const;
+};
+
+
+
+#endif //PAIVAYS_H

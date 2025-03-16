39c48cf96e6e1dd9bb6d73dbc94bcbbe3239d5fb
Sami Koponen <samikoponen4@gmail.com>
2025-03-16 12:03:29 +0200

    Viikko 2 T1 valmis

--- OOP-Viikko-2/T1/main.cpp
+++ OOP-Viikko-2/T1/main.cpp
@@ -1,7 +1,14 @@
 #include "Henkilo.h"
 
 int main() {
+    Henkilo tuntematon;
+    tuntematon.tulostaHenkilonTiedot();
 
+    Henkilo pekka("Pekka", 20);
+    pekka.tulostaHenkilonTiedot();
+
+    //Tällöin kutsutaan pareaametritonta rakentajaa
+    Henkilo henkilot[10];
 
     return 0;
 }
